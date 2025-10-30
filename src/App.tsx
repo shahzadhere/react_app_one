8e097b98d723cbc374e8552575139a6e7f84d10d
Shahzad Iqbal <shahzadfourxfour@gmail.com>
2025-10-30 12:29:54 -0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className='text-blue-500'>Hello World</h1>
-      <p>Developed by Shahzad iqbal</p>
+      <p>Developed by Shahzad iqbal. You can visit my website <a href="https://shahzadhere.github.io/react_app_one/" target="_blank">here</a></p>
     </>
   )
 }
