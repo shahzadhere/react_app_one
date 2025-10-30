08dcccceebe95698da5bedc9565941342a842b15
Shahzad Iqbal <shahzad.iqbal@premierbpo.com>
2025-10-30 12:05:07 -0700

    first commit for my react project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-500'>Hello World</h1>
+      <p>Developed by Shahzad iqbal</p>
+    </>
+  )
+}
+
+export default App
